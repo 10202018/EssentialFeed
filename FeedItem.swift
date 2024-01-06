4cb7e8f37fe8a736c241685ab407e537dabbf89a
10202018 <44626394+10202018@users.noreply.github.com>
2024-01-05 20:21:23 -0800

    Initial Commit

---
+++ FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jah Morris-Jones on 1/5/24.
+//
+
+import Foundation
+
+struct FeedItem {
+  let id: UUID
+  let description: String?
+  let location: String?
+  let image: URL
+  
+  // Create public initializer
+}
