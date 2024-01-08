4c4633440daa152641da672015538b373dd6f497
10202018 <44626394+10202018@users.noreply.github.com>
2024-01-08 11:16:29 -0800

    Make the HTTPClient a protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,33 +8,35 @@
 import XCTest
 
 class RemoteFeedLoader {
+  let client: HTTPClient
+  
+  init(client: HTTPClient) {
+    self.client = client
+  }
   func load() {
     // A common technique we see in testing to be able to mock Singletons is to
     // is to make the shared instance a variable
     // HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
-    HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+    client.get(from: URL(string: "https://a-url.com")!)
   }
 }
 
-class HTTPClient {
-  static var shared = HTTPClient()
-  
-  func get(from url: URL) {}
+protocol HTTPClient {
+  func get(from url: URL)
 }
 
 class HTTPClientSpy: HTTPClient {
+  var requestedURL: URL?
   
-  override func get(from url: URL) {
+  func get(from url: URL) {
     requestedURL = url
   }
-  var requestedURL: URL?
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
   func test_init_doesNotRequestDataFromURL() {
     let client = HTTPClientSpy()
-    HTTPClient.shared = client
-    _ = RemoteFeedLoader()
+    _ = RemoteFeedLoader(client: client)
     
     XCTAssertNil(client.requestedURL)
   }
@@ -42,8 +44,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   func test_load_requestDataFromURL() {
     // Arrange: "Given a client and a sut..."
     let client = HTTPClientSpy()
-    HTTPClient.shared = client
-    let sut = RemoteFeedLoader()
+    let sut = RemoteFeedLoader(client: client)
 
     // Act: "When we invoke `sut.load()`..."
     sut.load()
