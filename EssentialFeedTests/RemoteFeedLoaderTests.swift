fd481a78fa0ccbaa8ff42d15523e2e5f45857cb7
10202018 <44626394+10202018@users.noreply.github.com>
2024-01-08 12:13:31 -0800

    Refactor test to remove duplication

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -21,22 +21,13 @@ class RemoteFeedLoader {
 }
 
 protocol HTTPClient {
+  var requestedURL: URL? { get }
   func get(from url: URL)
-}
-
-class HTTPClientSpy: HTTPClient {
-  var requestedURL: URL?
-  
-  func get(from url: URL) {
-    requestedURL = url
-  }
 }
 
 class RemoteFeedLoaderTests: XCTestCase {
   func test_init_doesNotRequestDataFromURL() {
-    let url = URL(string: "https://a-url.com")!
-    let client = HTTPClientSpy()
-    _ = RemoteFeedLoader(client: client, url: url)
+    let (_, client) = makeSUT()
     
     XCTAssertNil(client.requestedURL)
   }
@@ -44,15 +35,29 @@ class RemoteFeedLoaderTests: XCTestCase {
   func test_load_requestDataFromURL() {
     // Arrange: "Given a client and a sut..."
     let url = URL(string: "https://a-given-url.com")!
-    let client = HTTPClientSpy()
-    let sut = RemoteFeedLoader(client: client, url: url)
+    let (sut, client) = makeSUT(url: url)
 
     // Act: "When we invoke `sut.load()`..."
     sut.load()
     
     // Assert: "Assert that a URL request was initiated in the client"
-    
     XCTAssertEqual(client.requestedURL, url)
+  }
+  
+// MARK: - Helpers
+
+  private class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
     
+    func get(from url: URL) {
+      requestedURL = url
+    }
+  }
+  
+  private func makeSUT(url: URL = URL(string: "https://a-url.com")!)
+  -> (sut: RemoteFeedLoader, client: HTTPClient) {
+    let client = HTTPClientSpy()
+    let remoteFeedLoader = RemoteFeedLoader(client: client, url: url)
+    return (remoteFeedLoader, client)
   }
 }
