e67156f133bb1c5c0f73a500e2e2641dccd7a3b1
10202018 <44626394+10202018@users.noreply.github.com>
2024-01-17 16:59:54 -0800

    Move mapping logic to a new type, `FeedItemsMapper`

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -39,9 +39,10 @@ public final class RemoteFeedLoader {
     client.get(from: url) { result in
       switch result {
       case let .success(data, response):
-        if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-          completion(.success(root.items.map({ $0.item })))
-        } else {
+        do {
+          let items = try FeedItemsMapper.map(data: data, response: response)
+          completion(.success(items))
+        } catch {
           completion(.failure(.invalidData))
         }
       case .failure:
@@ -51,18 +52,28 @@ public final class RemoteFeedLoader {
   }
 }
 
-private struct Root: Decodable {
-  let items: [Item]
-}
+private class FeedItemsMapper {
+  private struct Root: Decodable {
+    let items: [Item]
+  }
 
-private struct Item: Decodable {
-  var id: UUID
-  var description: String?
-  var location: String?
-  var image: URL
+  private struct Item: Decodable {
+    var id: UUID
+    var description: String?
+    var location: String?
+    var image: URL
+    
+    var item: FeedItem {
+      return FeedItem(id: id, description: description, location: location, imageURL: image)
+    }
+  }
   
-  var item: FeedItem {
-    return FeedItem(id: id, description: description, location: location, imageURL: image)
+  static func map(data: Data, response: HTTPURLResponse) throws -> [FeedItem] {
+    guard response.statusCode == 200 else {
+      throw RemoteFeedLoader.Error.invalidData
+    }
+    let root = try JSONDecoder().decode(Root.self, from: data)
+    return root.items.map { $0.item }
   }
 }
 
