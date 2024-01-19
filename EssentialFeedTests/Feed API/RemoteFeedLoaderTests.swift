d715a43fa34215d787b6096ae2446c5e6bea6e46
10202018 <44626394+10202018@users.noreply.github.com>
2024-01-19 11:43:21 -0800

    Simplify type declarations with a helper function.

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -43,7 +43,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   func test_load_deliversErrorOnClientError() {
     let (sut, client) = makeSUT()
     
-    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.connectivity)) {
+    expect(sut, toCompleteWith: failure(.connectivity)) {
       let clientError = NSError(domain: "Test", code: 0)
       client.complete(with: clientError)
     }
@@ -64,7 +64,7 @@ class RemoteFeedLoaderTests: XCTestCase {
   func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
     let (sut, client) = makeSUT()
     
-    expect(sut, toCompleteWith: .failure(RemoteFeedLoader.Error.invalidData), when: {
+    expect(sut, toCompleteWith: failure(.invalidData), when: {
       let invalidJSON = Data("invalid json".utf8)
       client.complete(withStatusCode: 200, data: invalidJSON)
     })
@@ -155,6 +155,9 @@ class RemoteFeedLoaderTests: XCTestCase {
     return (sut, client)
   }
   
+  private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+    return .failure(error)
+  }
   
   private func trackForMemoryLeaks(_ instance: AnyObject,  file: StaticString = #filePath, line: UInt = #line) {
     addTeardownBlock { [weak instance] in
